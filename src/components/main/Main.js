9e6b7a2057ac8387b19403e801505d542924d6fc
Jay Nirgudkar <jnirgudk@cisco.com>
2021-08-04 23:29:29 -0700

    updating basic react dashboard

---
+++ src/components/main/Main.js
@@ -0,0 +1,103 @@
+
+import "./Main.css"
+import hello from "../../assets/cisco.png"
+import Chart from "../charts/Chart"
+
+const Main = () => {
+
+    document.getElementById("date").innerHTML = new Date().toLocaleDateString()
+
+    return (
+        <main>
+            <div className="main__container">
+                <div className="main__title">
+                    <img src={hello} alt="hello"></img>
+                    <div className="main__greeting">
+                        <p id="date" ></p>
+                        <p>Welcome to Dashboard</p>
+                    </div>
+                </div>
+
+                <div className="main__cards">
+                    <div className="card">
+                        <i className="fa fa-user-o fa-2x text-lightblue"></i>
+                        <div className="card_inner">
+                            <p className="text-primary-p">Number of subscribers</p>
+                            <span className="font-bold text-title">578</span>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <i className="fa fa-calendar fa-2x text-red"></i>
+                        <div className="card_inner">
+                            <p className="text-primary-p">Times of watching</p>
+                            <span className="font-bold text-title"> 2567 </span>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <i className="fa fa-video-camera fa-2x text-yellow"></i>
+                        <div className="card_inner">
+                            <p className="text-primary-p">Number of Videos</p>
+                            <span className="font-bold text-title">340</span>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <i className="fa fa-thumbs-up fa-2x text-green"></i>
+                        <div className="card_inner">
+                            <p className="text-primary-p">Number of Likes</p>
+                            <span className="font-bold text-title">645</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="charts">
+                    <div className="charts__left">
+                        <div className="charts__left__title">
+                            <div>
+                                <h1>Daily reports</h1>
+                                <p>Cali</p>
+                            </div>
+                            <i className="fa fa-usd"></i>
+                        </div>
+                        <Chart/>
+                    </div>
+
+                    <div className="charts__right">
+                        <div className="charts__right__title">
+                            <div>
+                                <h1>Stats report</h1>
+                                <p>Cali</p>
+                            </div>
+                            <i className="fa fa-use"></i>
+                        </div>
+
+                        <div className="charts__right__cards">
+                            <div className="card1">
+                                <h1>Income</h1>
+                                <p>$75,000</p>
+                            </div>
+
+                            <div className="card1">
+                                <h1>Sales</h1>
+                                <p>$275,000</p>
+                            </div>
+
+                            <div className="card1">
+                                <h1>Users</h1>
+                                <p>5000</p>
+                            </div>
+
+                            <div className="card1">
+                                <h1>Orders</h1>
+                                <p>1200</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,80 @@
+import "./Sidebar.css"
+import logo from "../../assets/cisco.png"
+
+
+const Sidebar = ({sidebarOpen, closeSidebar}) => {
+    return (
+        <div className={sidebarOpen ? "sidebar-responsive" : ""} id="sidebar">
+            <div className="sidebar__title">
+                <div className="sidebar__img">
+                    <img src={logo} alt="logo"/>
+                    <h1>Codersbite</h1>
+                </div>
+                <i
+                    className="fa fa-times"
+                    id="sidebarIcon"
+                    onClick={() => closeSidebar()}>
+                </i>
+            </div>
+
+            <div className="sidebar__menu">
+                <div className="sidebar__link active_menu_link">
+                    <i className="fa fa-home"></i>
+                    <a href="/#">Dashboard</a>
+                </div>
+                <h2>Select Colos</h2>
+                <div className="sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 1</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 2</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 3</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 4</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 5</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 6</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 7</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 8</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 9</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 10</a>
+                </div>
+                <div className = "sidebar__link">
+                    <i className="fa"></i>
+                    <a href="/#">Colo 11</a>
+                </div>
+                <h2>Logout</h2>
+                <div className = "sidebar__link">
+                    <i className="fa fa-power-off"></i>
+                    <a href="/#">Log out</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
